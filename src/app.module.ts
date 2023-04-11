c940c7e1ee4343efef106577a9486960aaa776bc
Joao Vitor Lima Lago Santos <vorque45@gmail.com>
2023-04-11 15:54:48 -0300

    Adição de um novo modulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContaModule } from './modules/Conta/conta.module';
+import { ClienteModule } from './modules/Cliente/cliente.module';
 
 @Module({
-  imports: [ContaModule],
+  imports: [ContaModule, ClienteModule],
   controllers: [AppController],
   providers: [AppService],
 })
